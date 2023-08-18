b1e8fb00fbdd8081bf8bcdbb0d8c49a9817d1528
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 14:00:18 +0100

    Created Dao function to insert network data into database

--- app/src/main/java/com/example/randomuserapplication/db/UserDao.kt
+++ app/src/main/java/com/example/randomuserapplication/db/UserDao.kt
@@ -1,8 +1,14 @@
 package com.example.randomuserapplication.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertToDatabase(users: List<UserEntity>)
+
 
 }
