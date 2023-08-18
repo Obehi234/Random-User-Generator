af1302d2843b34807cf0c8d79fc0ffc9ebb43dff
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 12:29:06 +0100

    Created Constants Object to store base url and end point

---
+++ app/src/main/java/com/example/randomuserapplication/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.randomuserapplication.util
+
+object Constants {
+
+    const val BASE_URL = "https://randomuser.me/"
+    const val END_POINT = "api/?results=500&inc=name,picture,location,nat,gender,dob,login&noinfo"
+}
