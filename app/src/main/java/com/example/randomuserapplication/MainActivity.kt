fa8b533142a0bcf12fd675e28e9b0196333655bf
Obehi234 <48982222+Obehi234@users.noreply.github.com>
2023-08-18 08:44:14 +0100

    Added Screen 1 UI  (#2)
    * Created Navgraph with two screens
    
    * Added recyclerview to screen 1
    
    * Set up screen 1 UI
    
    * Set up Nav Controller
    
    * Replaced margin start/end with marginHorizontal
    
    * Updated UI for screen 1
    
    * Removed screen 2
    
    * Fixed buttons and added searchView
    
    * Extracted dimensions
    
    * Updated searchview
    
    * Removed all boiler plate code from Fragment

--- app/src/main/java/com/example/randomuserapplication/MainActivity.kt
+++ app/src/main/java/com/example/randomuserapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.randomuserapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.example.randomuserapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
     }
 }
