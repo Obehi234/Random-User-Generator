1233f2bd57c28a783ed0fc7e678e1fcbfd296214
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 13:58:15 +0100

    Created user database with corresponding dao and entity classes

---
+++ app/src/main/java/com/example/randomuserapplication/db/UserEntity.kt
@@ -0,0 +1,40 @@
+package com.example.randomuserapplication.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.randomuserapplication.util.Constants.USER_TABLE
+
+@Entity(tableName = USER_TABLE)
+data class UserEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    @ColumnInfo("Title")
+    val title:String,
+    @ColumnInfo("FirstName")
+    val firstName: String,
+    @ColumnInfo("LastName")
+    val lastName: String,
+    @ColumnInfo("Gender")
+    val gender: String,
+    @ColumnInfo("Age")
+    val dob: Int,
+    @ColumnInfo("StreetNumber")
+    val streetNumber: Int,
+    @ColumnInfo("StreetName")
+    val streetName: String,
+    @ColumnInfo("City")
+    val city: String,
+    @ColumnInfo("State")
+    val state: String,
+    @ColumnInfo("Country")
+    val country: String,
+    @ColumnInfo("Nationality")
+    val nat: String,
+    @ColumnInfo("Picture")
+    val pictureUrl: String,
+    @ColumnInfo("PictureLarge")
+    val pictureLarge: String,
+    @ColumnInfo("UserName")
+    val userName: String
+    )
--- app/src/main/java/com/example/randomuserapplication/util/Constants.kt
+++ app/src/main/java/com/example/randomuserapplication/util/Constants.kt
@@ -2,4 +2,6 @@ package com.example.randomuserapplication.util
 
 object Constants {
     const val BASE_URL = "https://randomuser.me/"
+    const val USER_TABLE = "user_table"
+    const val USER_DATABASE = "user_database"
 }
