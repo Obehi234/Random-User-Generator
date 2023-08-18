a22d735bd14173fb746fe91735bd7b93425be64a
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 13:30:30 +0100

    Optimized imports after extracting endpoint

--- app/src/main/java/com/example/randomuserapplication/network/UserService.kt
+++ app/src/main/java/com/example/randomuserapplication/network/UserService.kt
@@ -1,7 +1,6 @@
 package com.example.randomuserapplication.network
 
 import com.example.randomuserapplication.model.UserList
-import com.example.randomuserapplication.util.Constants.END_POINT
 import retrofit2.Response
 import retrofit2.http.GET
 
--- app/src/main/java/com/example/randomuserapplication/util/Constants.kt
+++ app/src/main/java/com/example/randomuserapplication/util/Constants.kt
@@ -2,5 +2,4 @@ package com.example.randomuserapplication.util
 
 object Constants {
     const val BASE_URL = "https://randomuser.me/"
-    const val END_POINT = "api/?results=500"
 }
