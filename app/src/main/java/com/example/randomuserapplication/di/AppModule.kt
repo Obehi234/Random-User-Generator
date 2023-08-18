2a7acd2c9c306d73ab9a27c1dc05d5940067e14b
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 12:59:09 +0100

    Set up dependencies for network call and logging

--- app/src/main/java/com/example/randomuserapplication/di/AppModule.kt
+++ app/src/main/java/com/example/randomuserapplication/di/AppModule.kt
@@ -1,11 +1,17 @@
 package com.example.randomuserapplication.di
 
 import com.example.randomuserapplication.network.UserService
+import com.example.randomuserapplication.util.Constants.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -21,6 +27,23 @@ object AppModule {
     @Singleton
     @Provides
     fun getRetrofitInstance() : Retrofit {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client: OkHttpClient = OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        val gson: Gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
 
     }
 
