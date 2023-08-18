bd09de946fcff3cc35b83c68df97f1c155033a07
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 12:51:20 +0100

    Created retrofi t service instance as a dependency

--- app/src/main/java/com/example/randomuserapplication/di/AppModule.kt
+++ app/src/main/java/com/example/randomuserapplication/di/AppModule.kt
@@ -1,15 +1,28 @@
 package com.example.randomuserapplication.di
 
+import com.example.randomuserapplication.network.UserService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun getRetrofitServiceInstance(retrofit: Retrofit) : UserService {
+        return retrofit.create(UserService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun getRetrofitInstance() : Retrofit {
+
+    }
 
 
 }
--- app/src/main/java/com/example/randomuserapplication/network/UserService.kt
+++ app/src/main/java/com/example/randomuserapplication/network/UserService.kt
@@ -1,4 +1,11 @@
 package com.example.randomuserapplication.network
 
+import com.example.randomuserapplication.model.UserList
+import com.example.randomuserapplication.util.Constants.END_POINT
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface UserService {
+    @GET(END_POINT)
+    suspend fun getAllUsers() : Response<UserList>
 }
