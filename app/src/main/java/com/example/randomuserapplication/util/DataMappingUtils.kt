d8c2fd96287e56bb0e0fe59cb62b6c278d4c4500
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 14:07:48 +0100

    Created data mapping object to map my extracted result into my UserEntity format for easier ientification in my database

---
+++ app/src/main/java/com/example/randomuserapplication/util/DataMappingUtils.kt
@@ -0,0 +1,25 @@
+package com.example.randomuserapplication.util
+
+import com.example.randomuserapplication.db.UserEntity
+import com.example.randomuserapplication.model.Result
+
+object DataMappingUtils {
+    fun mapResultToUserEntity(result:Result): UserEntity {
+        return UserEntity(
+            city = result.location.city,
+            country = result.location.country,
+            state = result.location.state,
+            streetName = result.location.street.name,
+            streetNumber = result.location.street.number,
+            title = result.name.title,
+            firstName = result.name.first,
+            lastName = result.name.last,
+            gender = result.gender,
+            dob = result.dob.age,
+            nat = result.nat,
+            pictureUrl = result.picture.thumbnail,
+            pictureLarge = result.picture.medium,
+            userName = result.login.username
+        )
+    }
+}
