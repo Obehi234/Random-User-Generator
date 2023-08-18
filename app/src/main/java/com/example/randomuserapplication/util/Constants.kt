ec8c25db19e7cc01e82d7505e8886854acb3f942
Obehi234 <obehi.ataga234@gmail.com>
2023-08-18 13:17:14 +0100

    Modified end point

--- app/src/main/java/com/example/randomuserapplication/util/Constants.kt
+++ app/src/main/java/com/example/randomuserapplication/util/Constants.kt
@@ -2,5 +2,5 @@ package com.example.randomuserapplication.util
 
 object Constants {
     const val BASE_URL = "https://randomuser.me/"
-    const val END_POINT = "api/?results=500&inc=name,picture,location,nat,gender,dob,login&noinfo"
+    const val END_POINT = "api/?results=500"
 }
