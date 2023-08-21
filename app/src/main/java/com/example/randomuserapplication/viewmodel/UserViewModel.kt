194e5403b115bc36fe36148b620c4c8aa719f2dd
Obehi234 <48982222+Obehi234@users.noreply.github.com>
2023-08-21 13:21:36 +0100

    View model setup (#6)
    * Created viewmodel class
    
    * Created function to retrieve data fromApi and add to database
    
    * Created expression to extract data from database

---
+++ app/src/main/java/com/example/randomuserapplication/viewmodel/UserViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.randomuserapplication.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.randomuserapplication.db.UserEntity
+import com.example.randomuserapplication.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UserViewModel @Inject constructor(
+   private val userRepository: UserRepository
+) : ViewModel() {
+
+    fun addUsersToDatabase() {
+        viewModelScope.launch {
+            userRepository.fetchAndSaveUsers()
+        }
+    }
+
+    val usersList: List<UserEntity> = userRepository.getUsersFromDatabase()
+
+}
