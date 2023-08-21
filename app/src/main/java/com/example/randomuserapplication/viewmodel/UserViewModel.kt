08fcb222adda03347e7707c0820058f731cb2aba
Obehi234 <obehi.ataga234@gmail.com>
2023-08-21 10:14:02 +0100

    Created function to retrieve data fromApi and add to database

--- app/src/main/java/com/example/randomuserapplication/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/randomuserapplication/viewmodel/UserViewModel.kt
@@ -1,14 +1,26 @@
 package com.example.randomuserapplication.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.randomuserapplication.db.UserEntity
 import com.example.randomuserapplication.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UserViewModel @Inject constructor(
-    userRepository: UserRepository
+   private val userRepository: UserRepository
 ) : ViewModel() {
+
+    val usersList: List<UserEntity> = userRepository.getUsersFromDatabase()
+
+    fun addUsersToDatabase() {
+        viewModelScope.launch {
+            userRepository.fetchAndSaveUsers()
+        }
+    }
 
 
 }
