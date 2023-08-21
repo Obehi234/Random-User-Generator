da394a0492bde63854b52c3ec9585e576db5b020
Obehi234 <48982222+Obehi234@users.noreply.github.com>
2023-08-21 14:29:06 +0100

    Adapter setup (#7)
    * Created User Adapter class
    
    * Adapter class created successfully
    
    * Replaced individual binding statements with binding.apply
    
    * Removed all extra variables

---
+++ app/src/main/java/com/example/randomuserapplication/adapter/UserAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.randomuserapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.randomuserapplication.databinding.UserGridBinding
+import com.example.randomuserapplication.db.UserEntity
+import com.squareup.picasso.Picasso
+
+class UserAdapter : ListAdapter<UserEntity, UserAdapter.UserViewHolder>(UserDiffCallback())  {
+
+    inner class UserViewHolder(private val binding: UserGridBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: UserEntity) {
+            binding.apply {
+                Picasso.with(root.context)
+                    .load(item.pictureUrl)
+                    .into(profileImage)
+
+                userCity.text = "${item.city}, ${item.country}"
+
+                profileName.text = "${item.firstName} ${item.lastName}"
+
+                followBtn.setOnClickListener {
+                    followBtn.text = "Following"
+                    Toast.makeText(root.context, "Follow successful!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+        }
+    }
+
+    class UserDiffCallback : DiffUtil.ItemCallback<UserEntity>() {
+        override fun areItemsTheSame(oldItem: UserEntity, newItem: UserEntity): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: UserEntity, newItem: UserEntity): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = UserGridBinding.inflate(inflater, parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
