92e647799bb1a851dd95e8a9de4b246aa5bc1451
Obehi234 <obehi.ataga234@gmail.com>
2023-08-21 14:47:10 +0100

    Initialized userViewmodel by activity viewmodel

--- app/src/main/java/com/example/randomuserapplication/presentation/ui/UserListFragment.kt
+++ app/src/main/java/com/example/randomuserapplication/presentation/ui/UserListFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.randomuserapplication.adapter.UserAdapter
 import com.example.randomuserapplication.databinding.FragmentUserListBinding
+import com.example.randomuserapplication.viewmodel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserListFragment : Fragment() {
     private var _binding: FragmentUserListBinding?= null
     private val binding get() = _binding!!
+    private lateinit var rvAdapter: UserAdapter
+    private val userViewModel: UserViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,7 @@ class UserListFragment : Fragment() {
         _binding = FragmentUserListBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+
 
 }
