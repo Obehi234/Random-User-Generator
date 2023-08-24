21e0529a6d275c1386a1b4962124d734456a8236
Obehi234 <obehi.ataga234@gmail.com>
2023-08-24 18:53:13 +0100

    created an event listener to observe for text changes in the search bar

--- app/src/main/java/com/example/randomuserapplication/presentation/ui/UserListFragment.kt
+++ app/src/main/java/com/example/randomuserapplication/presentation/ui/UserListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -34,6 +35,15 @@ class UserListFragment : Fragment() {
             val action = UserListFragmentDirections.actionUserListFragmentToUserDetailsFragment(userId)
             findNavController().navigate(action)
         }
+        binding.searchBar.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+        })
 
     }
 
