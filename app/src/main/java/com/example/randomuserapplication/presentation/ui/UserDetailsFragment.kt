ee80cda65cab6fbdd4bd869ec2bd8dd548b0f46a
Obehi234 <obehi.ataga234@gmail.com>
2023-08-24 17:01:51 +0100

    Removed selectedUser variable from viewmodel as it is unnecessary due to safe args use

--- app/src/main/java/com/example/randomuserapplication/presentation/ui/UserDetailsFragment.kt
+++ app/src/main/java/com/example/randomuserapplication/presentation/ui/UserDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.randomuserapplication.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,7 +29,9 @@ class UserDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         userId = requireArguments().getString("userId") ?: ""
+        Log.d("CHECK_USER_ID", "USER_ID - $userId")
         viewLifecycleOwner.lifecycleScope.launch {
             val user = userViewModel.getSingleUser(userId)
             binding.apply{
